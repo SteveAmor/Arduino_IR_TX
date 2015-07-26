786a5ec2b8dd261f944aeb6396a47e9121bb007b
SteveAmor <SteveAmor@users.noreply.github.com>
2015-07-26 17:58:57 +0100

    If STB is already on, turn it off first

--- IRsendLoungeTVandBTVSTB.ino
+++ IRsendLoungeTVandBTVSTB.ino
@@ -20,38 +20,43 @@ void loop() {
   int STB_Status = digitalRead(4);
   digitalWrite(13, STB_Status);
 
-  if(!STB_Status) {  
+  if(STB_Status) {  
     for (int i = 0; i < 3; i++) {
-      irsend.sendRCMM(0x2420260C, 32); // BT Vision STB power code
+      irsend.sendRCMM(0x2420260C, 32); // BT Vision STB power code (Turn off)
       delay(91);
     }
+  delay(2000);
   }
-    delay(2000);
-    for (int i = 0; i < 3; i++) {
-      irsend.sendNEC(0x00FE02FD, 32); // Lounge TV power code
-      delay(40);
-    }
-    
+  
+  for (int i = 0; i < 3; i++) {
+    irsend.sendRCMM(0x2420260C, 32); // BT Vision STB power code (Turn on)
+    delay(91);
+  }  
+
   if(!STB_Status) { 
-    delay(1000);
-    for (int i = 0; i < 3; i++) {
-      irsend.sendRCMM(0x2420A601, 32); // BT Vision CH1 code
-      delay(91);
-    }
-    delay(1000);
-    for (int i = 0; i < 3; i++) {
-      irsend.sendRCMM(0x2420A65C, 32); // BT Vision Select code
-      delay(91);
-    }
-    delay(1000);
-    for (int i = 0; i < 3; i++) {
-      irsend.sendRCMM(0x2420A65C, 32); // BT Vision Select code
-      delay(91);
-    }
+    delay(2000);  
+  }  
+
+  for (int i = 0; i < 3; i++) {
+    irsend.sendNEC(0x00FE02FD, 32); // Lounge TV power code (Turn on)
+    delay(40);
   }
-  
-  STB_Status = digitalRead(4);
-  digitalWrite(13, STB_Status); 
-  
+    
+  delay(1000);
+  for (int i = 0; i < 3; i++) {
+    irsend.sendRCMM(0x2420A601, 32); // BT Vision CH1 code
+    delay(91);
+  }
+  delay(1000);
+  for (int i = 0; i < 3; i++) {
+    irsend.sendRCMM(0x2420A65C, 32); // BT Vision Select code
+    delay(91);
+  }
+  delay(1000);
+  for (int i = 0; i < 3; i++) {
+    irsend.sendRCMM(0x2420A65C, 32); // BT Vision Select code
+    delay(91);
+  }
+ 
   while(1) digitalWrite(13, digitalRead(4)) ; // repeat forever
 }
